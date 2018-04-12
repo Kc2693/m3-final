ca9cb8c83371420c816c00fea24fb74c70271416
Kailin Cannon <kailin.cannon@gmail.com>
2018-04-12 15:22:55 -0600

    Styling done

--- src/containers/Cards/Card.js
+++ src/containers/Cards/Card.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
 import * as API from '../../Api/Api';
-import { connect } from 'react-redux';
-import * as actions from '../../actions/actions';
 import './card.css';
 
 class Card extends Component {
-
-  handleClick = async () => {
-    const pokemonToId = this.props.data.pokemon
-    const response = await API.getPokeIds(pokemonToId)
+  constructor(props) {
+    super(props)
+    this.state = {
+      pokemon: [],
+      clicked: false
+    }
   }
 
-  displayPokemon = () => {
+  handleClick = async (event) => {
+    const pokemonToId = this.props.data.pokemon
+    const response = await API.getPokeIds(pokemonToId)
 
+    if(!this.state.clicked) {
+      this.setState({
+        clicked: true,
+        pokemon: response
+      })
+    } else {
+      this.setState({
+        clicked: false,
+      })
+    }
+
+  }
+
+  displayPokemon = (pokemon) => {
+    return pokemon.map((pokemon, index) => {
+      return (
+        <div className="poke-info" key={index}>
+          <p>Name: {pokemon.name}</p>
+          <p>Type: {this.props.data.name}</p>
+          <p>Weight: {pokemon.weight}</p>
+          <img src={pokemon.sprites.front_default} alt="poke-sprite" />
+        </div>
+      )
+    })
   }
 
   render(props) {
     return (
       <div
-        className="poke-card"
+        className={this.state.clicked ? "active-card" : "poke-card"}
         onClick={this.handleClick}>
         <h5>{this.props.data.name}</h5>
+        <div className="poke-info-card">
+          {this.state.clicked && this.displayPokemon(this.state.pokemon)}
+        </div>
       </div>
     )
   }
