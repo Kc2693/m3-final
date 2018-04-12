c4effb328f8de3cf8cb33820156ec54ce5d0bfa2
Kailin Cannon <kailin.cannon@gmail.com>
2018-04-12 10:00:15 -0600

    I don't know how to path

---
+++ src/components/Cards/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Card = () => {
+  return (
+    <div>This is a card</div>
+  )
+}
--- src/containers/PokedexWrapper/PokedexWrapper.js
+++ src/containers/PokedexWrapper/PokedexWrapper.js
@@ -3,12 +3,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import * as API from '../../Api/Api';
+import loadingGif from './loading.gif';
+import { Card } from '../../components/Cards/Card';
 
 class PokedexWrapper extends Component {
 
   getPokes = async() => {
     const pokemon = await API.fetchPokeTypes();
     this.props.storePokemon(pokemon)
+  }
+
+  displayElements = () => {
+    this.props.pokemon.length > 10 ?
+      this.makeCards() :
+      this.displayLoadingGif()
+  }
+
+  makeCards = () => {
+    return this.props.pokemon.map((pokemon) => {
+      <Card data={pokemon} />
+    })
+  }
+
+  displayLoadingGif = () => {
+    return (
+      <img src={loadingGif} alt="pikachu running"/>
+    )
   }
 
   render() {
